9e99019e199e57fc786faa6a6d0e71860913c6d6
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2015-07-12 02:22:11 +0300

    Fix dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -5,18 +5,19 @@ import sbt.Keys._
 object Build extends MechaRepoBuild {
   lazy val buildSettings = Defaults.coreDefaultSettings ++
     MechaRepoPlugin.defaultSettings ++ Seq(
-    name := "finch-docker",
+    name := "finagle-docker",
     scalaVersion := "2.11.7",
     version := "0.1",
     organization := "com.cleawing",
-    libraryDependencies ++= superRepoDependencies("finch-docker") ++
-      Seq(Dependencies.scalaz, Dependencies.scalaTest, Dependencies.scalazScalaTest)
+    libraryDependencies ++= superRepoDependencies("finagle-docker") ++
+      Seq(Dependencies.scalaz, Dependencies.json4s, Dependencies.jackson,
+        Dependencies.scalaTest, Dependencies.scalazScalaTest)
   )
 
-  def repoName = "finch-docker"
+  def repoName = "finagle-docker"
 
   lazy val finagleServices: Project = Project(
-    "finch-docker",
+    "finagle-docker",
     file("."),
     settings = buildSettings
   ) dependsOnSuperRepo
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,15 @@ import sbt._
 object Dependencies {
   object Versions {
     val scalaz = "7.1.3"
+    val json4s = "3.3.0.RC2"
+    val jackson = "2.5.4"
     val scalazScalaTest = "0.2.3"
     val scalaTest = "2.2.5"
   }
 
   lazy val scalaz  = "org.scalaz" %% "scalaz-core" % Versions.scalaz
+  lazy val json4s = "org.json4s" %% "json4s-jackson" % Versions.json4s
+  lazy val jackson = "com.fasterxml.jackson.core" % "jackson-databind" % Versions.jackson
   lazy val scalazScalaTest  = "org.typelevel" %% "scalaz-scalatest" % Versions.scalazScalaTest % "test"
   lazy val scalaTest  = "org.scalatest" %%  "scalatest" % Versions.scalaTest % "test"
 }
